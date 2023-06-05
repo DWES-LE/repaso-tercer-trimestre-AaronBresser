949bdb78c330953bacd79f0a80d55e5cf7e0a256
AaronBresser <aaronbresser02@gmail.com>
2023-06-05 08:47:20 +0200

    Finalización del repaso wagtail

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-b+h!g_(v3=dx00a0n3r&ef__!7no!#$n$t!gtw#c9qcp5q4t42"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
